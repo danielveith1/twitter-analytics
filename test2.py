2014e729ead5de2adcde5da4ee841b45c7a3ad04
danveith1 <dev6500@yahoo.com>
2015-10-01 11:52:44 -0400

    obtained retweet count and date of tweet

--- test2.py
+++ test2.py
@@ -1,11 +1,18 @@
 #!/Anaconda/python
 from password import *
 import twitter
+import time
 api = twitter.Api(consumer_key=ckey,
                       consumer_secret=csecret,
                       access_token_key=atoken,
                       access_token_secret=asecret)
-print api.VerifyCredentials()
-					  
-statuses = api.GetUserTimeline(screen_name=user)
-#print [s.text for s in statuses]					  
+#print api.VerifyCredentials()
+#user = input("What twitter user do you want to look up?")					  
+statuses = api.GetUserTimeline(screen_name="RealHughJackman")
+#print [s.text for s in statuses]
+#print [dir(s) for s in statuses]					  
+print [s.retweet_count for s in statuses]
+#Retweeted_status and FavoriteCount
+#print [s.retweeted_status for s in statuses]
+#ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
+print [time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(s.created_at,'%a %b %d %H:%M:%S +0000 %Y')) for s in statuses]
