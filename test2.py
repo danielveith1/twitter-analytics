155a6208382c8b37683e514dff82af2fa67aae91
danveith1 <dev6500@yahoo.com>
2015-10-01 14:37:21 -0400

    commented out graphing
    Until debugged, graphing portion will be commented out.

--- test2.py
+++ test2.py
@@ -2,54 +2,69 @@
 from password import *
 import twitter
 import time
+from datetime import date
+import datetime 
 import math
-import matplotlib 
-import matplotlib.pyplot
+import random
+import matplotlib.pyplot as plt
+from matplotlib.finance import quotes_historical_yahoo_ochl
+from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
+import datetime
 import types
 import numpy
-import bisect
-def show_plot(arg1, arg2=None):
 
-    def real_decorator(f):
-        def wrapper(*args, **kwargs):
-            matplotlib.pyplot.figure(figsize=(arg1, arg2))
-            result = f(*args, **kwargs)
-            matplotlib.pyplot.show()
-            return result
-        return wrapper
-
-    if type(arg1) == types.FunctionType:
-        f = arg1
-        arg1, arg2 = 10, 5
-        return real_decorator(f)
-    return real_decorator
+# authenticates the twitter app	
 api = twitter.Api(consumer_key=ckey,
                       consumer_secret=csecret,
                       access_token_key=atoken,
                       access_token_secret=asecret)
+#verifyCredentials only necessary to verify authentication
 #print api.VerifyCredentials()
-#user = input("What twitter user do you want to look up?")					  
+
+# obtaining timeline from a specific user					  
 statuses = api.GetUserTimeline(screen_name="RealHughJackman")
-#print [s.text for s in statuses]
+# used dir(s) to find all the attributes of statuses that is retrived from the timeline
 #print [dir(s) for s in statuses]					  
 print [s.retweet_count for s in statuses]
-#Retweeted_status and FavoriteCount
-#print [s.retweeted_status for s in statuses]
+#FavoriteCount is another attribute that would be useful to analyze
+
 #ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
 print [time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(s.created_at,'%a %b %d %H:%M:%S +0000 %Y')) for s in statuses]
 
 
-@show_plot
-def simple():
-    x_data = numpy.linspace(0., 100., 1000)
-
+'''
     for s in statuses:
-		x = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(s.created_at,'%a %b %d %H:%M:%S +0000 %Y'))
+		x = time.strftime('%Y-%m-%d', time.strptime(s.created_at,'%a %b %d +0000 %Y'))
 		y = s.retweet_count
-		matplotlib.pyplot.scatter(x,y)
 
-    axes = matplotlib.pyplot.gca()
-    axes.set_xlabel('Date')
-    axes.set_ylabel('Number of tweets')
+date1 = datetime.date(2015, 1, 1)
+date2 = datetime.date(2015, 12, 12)
 
-simple()
+years = YearLocator()   # every year
+months = MonthLocator()  # every month
+yearsFmt = DateFormatter('%Y')
+
+
+
+
+dates = [time.strptime(s.created_at,'%a %b %d %H:%M:%S +0000 %Y') for s in statuses]
+opens = [s.retweet_count for s in statuses]
+
+fig, ax = plt.subplots()
+ax.plot_date(dates, opens, '-')
+
+# format the ticks
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(yearsFmt)
+ax.xaxis.set_minor_locator(months)
+ax.autoscale_view()
+
+# format the coords message box
+def price(x): return '$%1.2f'%x
+ax.fmt_xdata = DateFormatter('%Y-%m-%d')
+ax.fmt_ydata = price
+ax.grid(True)
+
+fig.autofmt_xdate()
+plt.show()
+'''
