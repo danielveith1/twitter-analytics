2944c757e043385cc78de524381180b6ae3cebfc
danveith1 <dev6500@yahoo.com>
2015-10-01 12:23:12 -0400

    still debugging date plotting

--- test2.py
+++ test2.py
@@ -2,6 +2,27 @@
 from password import *
 import twitter
 import time
+import math
+import matplotlib 
+import matplotlib.pyplot
+import types
+import numpy
+import bisect
+def show_plot(arg1, arg2=None):
+
+    def real_decorator(f):
+        def wrapper(*args, **kwargs):
+            matplotlib.pyplot.figure(figsize=(arg1, arg2))
+            result = f(*args, **kwargs)
+            matplotlib.pyplot.show()
+            return result
+        return wrapper
+
+    if type(arg1) == types.FunctionType:
+        f = arg1
+        arg1, arg2 = 10, 5
+        return real_decorator(f)
+    return real_decorator
 api = twitter.Api(consumer_key=ckey,
                       consumer_secret=csecret,
                       access_token_key=atoken,
@@ -16,3 +37,19 @@ print [s.retweet_count for s in statuses]
 #print [s.retweeted_status for s in statuses]
 #ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
 print [time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(s.created_at,'%a %b %d %H:%M:%S +0000 %Y')) for s in statuses]
+
+
+@show_plot
+def simple():
+    x_data = numpy.linspace(0., 100., 1000)
+
+    for s in statuses:
+		x = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(s.created_at,'%a %b %d %H:%M:%S +0000 %Y'))
+		y = s.retweet_count
+		matplotlib.pyplot.scatter(x,y)
+
+    axes = matplotlib.pyplot.gca()
+    axes.set_xlabel('Date')
+    axes.set_ylabel('Number of tweets')
+
+simple()
