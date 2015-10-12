ab22fcba7952b4fcf820cfb1d0764ca6836c9322
danveith1 <dev6500@yahoo.com>
2015-10-11 21:25:22 -0400

    Added tweet text and tweet favorite count and made it prettier

--- index.php
+++ index.php
@@ -11,27 +11,42 @@ $content = $connection->get("account/verify_credentials");
 ?>
 <!DOCTYPE html>
 <html>
-<body>
-<p>
+	<head>
+		<title>Twitter App</title>
+		
+		<link type="text/css" rel="stylesheet" href="index.css"/>
+		<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro|Roboto+Condensed|Oxygen"/>
+	</head>
+	<body>
+		<p class= "table_names">
 <?php
 
-echo "The Tweets of RealHughJackman and the retweets they received.\n";
+echo "RealHughJackman's Tweets and Analytics:\n";
 $timeline = $connection->get("statuses/user_timeline", array("screen_name" => "RealHughJackman", "count" => 50, "exclude_replies" => true ));
 //$connection->get("statuses/home_timeline", array("screen_name=RealHughJackman"));
 //echo count($timeline);
-
-foreach($timeline as $value){
+?>
+<div id="content">
+			<table class="table table-bordered table-condensed table-hover table-striped">
+				<tr>
+					<th>Tweet Body</th>
+					<th>Date Of Tweet</th>
+					<th>Number of Retweets</th>
+					<th>Number of times Tweet has been Favorited</th>
+				</tr>
 	
-	echo "<p>";
-	echo "On ";
-	echo $value->created_at;
-	echo " the tweet received "; 
-	echo $value->retweet_count;
-	echo " retweets.";
-	
-	
-	
-}
+				<?php foreach($timeline as $value){?>
+				<tr>
+					<td><?php echo $value->text;?></td>
+					<td><?php printf($value->created_at); ?></td>
+					<td> <?php printf($value->retweet_count); ?> </td>
+					<td> <?php printf($value->favorite_count); ?> </td>
+                                                        
+				</tr>
+				<?php } ?>
+                                        
+			</table>
+<?php		
 
 $statuses = $connection->get("statuses/home_timeline", array("count" => 25, "exclude_replies" => true));
 /*
