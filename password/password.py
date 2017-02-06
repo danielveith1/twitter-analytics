33c0de4b98e222589b070d8f80c75c4224355805
danveith1 <dev6500@yahoo.com>
2017-02-06 10:16:43 -0500

    Deleted file that should not be in git.

--- password/password.py
+++
@@ -1,4 +0,0 @@
-ckey = 'ZPjhrCHccUXINwBXBl2xuxJ7i'
-csecret = 'o6tgck6aZ5NT4NWNjJvS2wBnEstZFXaxVUm8uiwbqGvwGaAadh'
-atoken = '2180964727-WOXSHn44NaZEqZK8rzyyj8DCEYeIjF6sIiLcLM8'
-asecret = 'RqlxCqU75YjNHbU4oQvDfUrHe8KRcV7TXZiDpmFN3i8Fd'
